a8c99bb1693f50d60f7e7467921e529759e2e4a1
ttsingh <125217412+ttsingh@users.noreply.github.com>
2023-03-26 17:55:36 -0700

    1

--- sketch.js
+++ sketch.js
@@ -18,16 +18,16 @@ function setup() {
     //this is how to use p5.js and javascript to generate images into html elements
 
     //create an image element in html
-    let cat = createImg("images/cat.png");
+    //let cat = createImg("images/cat.png");
 
     //select size of image
-    cat.size(200,200);
+    //cat.size(200,200);
 
     //set the position
-    cat.position(500, 500);
+   // cat.position(500, 500);
 
     // give it an id
-    cat.id('cat');
+    //cat.id('cat');
 
     //now you can see that in your html, an <img> tag is created that has an id of "cat", has a size of 200x200, and is positioned at 500x500 on the page. 
 
@@ -39,12 +39,12 @@ function setup() {
 
 
     //lets make this cat move without leaving a trail - removeElements(); up at the top
-    cat.position((200 + a), 500);
+    //cat.position((200 + a), 500);
 
     // a++;
 
     //make the bear appear when pressed
-    cat.mousePressed(bear)
+    //cat.mousePressed(bear)
 
   }
   
