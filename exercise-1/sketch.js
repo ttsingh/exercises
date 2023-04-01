d78b78ac3f81e8a980679838639b355b9723517b
ttsingh <125217412+ttsingh@users.noreply.github.com>
2023-04-01 13:27:34 -0700

    both

--- exercise-1/sketch.js
+++ exercise-1/sketch.js
@@ -8,7 +8,7 @@ function setup() {
 
   }
   
-  function draw() {
+  function draw(){
     // removeElements();
 
     //draw a white circle in the middle of the page
@@ -20,22 +20,23 @@ function setup() {
     //create an image element in html
     //let cat = createImg("images/cat.png");
 
-    let couch=createImg("../images/trash-couch.jpg")
+    let couch=createImg("images/trash-couch.jpg")
 
     //select size of image
     //cat.size(200,200);
-    //couch.size(200, 200)
+    couch.size(500, 500)
     //set the position
    // cat.position(500, 500);
-   //couch.position(500, 500);
+   couch.position(300, 300);
 
     // give it an id
     //cat.id('cat');
+    couch.id('couch')
 
     //now you can see that in your html, an <img> tag is created that has an id of "cat", has a size of 200x200, and is positioned at 500x500 on the page. 
 
     //lets make this follow your cursor
-    //cat.position(mouseX, mouseY);
+   // couch.position(mouseX, mouseY);
 
     //lets make this cat move
     //cat.position((200 + a), 500);
@@ -43,33 +44,35 @@ function setup() {
     couch.position(mouseX, mouseY);
     //lets make this cat move without leaving a trail - removeElements(); up at the top
     //cat.position((200 + a), 500);
-
-    // a++;
+    couch.position((200 + a), 500);
+     a++;
 
     //make the bear appear when pressed
     //cat.mousePressed(bear)
+    couch.mousePressed(racoon)
 
-    //let couch;
-    //function prelaod(){
-     // couch=loadImage("../images/trash-couch.jpg")
-    //}
-    //function setup(){
-      //createCanvas(400, 400);
-    //}
-    //function draw(){
-      //background(255);
-      //Image(couch, mouseX, mouseY);
+    let couch;
+    function prelaod(){
+      couch=loadImage("../images/trash-couch.jpg")
+    }
+    function setup(){
+      createCanvas(400, 400);
+    }
+    function draw(){
+      background(255);
+      Image(couch, mouseX, mouseY);
     //}
 
+    }
   }
-  
   function windowResized() {
     // resize the canvas when the screen is resized.
     resizeCanvas(windowWidth, windowHeight);
 }
 
-//function bear(){
-  //let bear = createImg("images/bearhug.png");
-  //bear.size(400,400);
-  //bear.position(800, 400);
-//}
+
+function racoon(){
+  let racoon = createImg("images/raccon.jpg");
+  racoon.size(400,400);
+  racoon.position(800, 400);
+}
